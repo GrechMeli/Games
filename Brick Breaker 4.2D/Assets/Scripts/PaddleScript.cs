820e4b5ac7898929fa09053141a0c63a21c1da46
Earvin John Grech Meli <b43015@mcast.org.mt>
2017-11-10 14:24:47 +0100

    - set paddle to move with mouse position in game units -Locked the ball to move with the paddle at the start of the game

---
+++ Brick Breaker 4.2D/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        print(Input.mousePosition.x);
+
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16)-8;
+
+        Vector3 newPaddlePos = new Vector3(mousePosInUnits, this.transform.position.y, 
+                                            this.transform.position.z);
+
+        newPaddlePos.x = Mathf.Clamp(mousePosInUnits, -7.36f, 7.36f);//doesn't let the paddle go past this point
+
+        this.transform.position = newPaddlePos;
+	}
+}
